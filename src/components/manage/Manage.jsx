f7644bb7991b5be635fde84e32e04ab70f26829d
HunterLi2001 <1398428436@qq.com>
2023-04-20 14:33:43 +0800

    feat: 添加管理页面基础样式

--- src/components/manage/Manage.jsx
+++ src/components/manage/Manage.jsx
@@ -1,8 +1,13 @@
 import styles from "@/styles/Manage.module.scss";
-import {Button, Collapse, Dropdown, Input, message, Space, Table} from "antd";
-import {DownOutlined} from "@ant-design/icons"
+import {Button, Collapse, Dropdown, Input, message, Space, Table, theme} from "antd";
+import {DownOutlined, SearchOutlined} from "@ant-design/icons";
 import deleteConnection from "@/utils/deleteConnection";
+
 export default function Manage(props) {
+    const {
+        token: {colorBgContainer}
+    } = theme.useToken();
+
     const items = [
         {
             label: "sadfsad",
@@ -29,12 +34,22 @@ export default function Manage(props) {
             <div className={styles.title}>
                 <span>{props.missionName.current}</span>
                 <Button
-                    onClick={() => {deleteConnection(props.connectionId.current, props.fetchData);}}
+                    onClick={() => {
+                        deleteConnection(props.connectionId.current, props.fetchData);
+                    }}
                     className={styles.delete_connection_btn}
                 >删除任务</Button></div>
-            <div>属性类别（点击切换标签类别）</div>
-            <Button>导入</Button><Button>导入数据</Button>
-            <Collapse>
+            <div className={styles.property_box}>
+                <div className={styles.property_type}>属性类别（点击切换标签类别）</div>
+                <div className={styles.btn_box}>
+                    <Button>导入</Button>
+                    <Button className={styles.import_data_btn}>导入数据</Button>
+                </div>
+            </div>
+            <Collapse
+                expandIconPosition="end"
+                style={{backgroundColor: colorBgContainer}}
+            >
                 <Collapse.Panel key={1} header={
                     <>
                         <span>名词类</span>
@@ -46,21 +61,29 @@ export default function Manage(props) {
                     </>
                 }>456</Collapse.Panel>
             </Collapse>
-            <div>数据展示</div>
-            <Button>+</Button>
-            {/*<div>姓名</div>*/}
-            <Dropdown menu={menu}>
-                <Button>
-                    <Space>
-                        姓名
-                        <DownOutlined/>
-                    </Space>
-                </Button>
-            </Dropdown>
-            {/*上面是下拉菜单*/}
-            <Input/>
-            <Button>搜索</Button>
-            <Table></Table>
+            <div className={styles.show_data_box}>
+                <div className={styles.show_data_title}>数据展示</div>
+                <Button size={"small"} type={"primary"} className={styles.plus_btn}>+</Button>
+                <div className={styles.search_bigbox}>
+                    <Dropdown menu={menu} className={styles.dropdown}>
+                        <Button>
+                            <Space>
+                                姓名
+                                <DownOutlined/>
+                            </Space>
+                        </Button>
+                    </Dropdown>
+                    <Input
+                        className={styles.search_box}
+                        placeholder={"你想查询的内容..."}
+                    />
+                    <Button className={styles.search_btn} type={"primary"}><SearchOutlined /></Button>
+                </div>
+            </div>
+            <Table
+                className={styles.table}
+                size={"large"}
+            />
         </div>
     );
 }
