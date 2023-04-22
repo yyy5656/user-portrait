39c86bd0e0c714dfc9d64df090a955e0e654da6f
yy <you@example.com>
2023-04-22 18:59:47 +0800

    feat: 完善图表信息栏

--- src/components/echar/BasicBar.jsx
+++ src/components/echar/BasicBar.jsx
@@ -5,28 +5,41 @@ import { useEffect, useRef } from "react";
 import { getCharOption } from "./constant";
 
 export default function BasicBar(props) {
-  console.log(props);
+  console.log("123", props);
   const { name, property, type, data } = props.charOption;
   const dom = useRef();
 
   // const [myChart, setMyChart] = useState();
 
   useEffect(() => {
+    let option = getCharOption(type, data, name, property[0].linkComment);
+    console.log("123", option);
+  }, []);
+
+  useEffect(() => {
     const myChart = echarts.init(dom.current, null, {
       width: 500,
       height: 400,
     });
-    const option = getCharOption(type, data, name, property[0].linkComment);
-    console.log(option);
+    let option = getCharOption(type, data, name, property[0].linkComment);
+    console.log("123", option);
     option && myChart.setOption(option);
+    return () => {
+      option = null;
+    };
   }, [dom.current]);
 
   return (
     <>
       <div className={styles.basicBar_container}>
-        <div className={styles.cancel} onClick={()=>{
-          props.deleteViewChar(props.viewId)
-        }}><CloseOutlined /></div>
+        <div
+          className={styles.cancel}
+          onClick={() => {
+            props.deleteViewChar(props.viewId);
+          }}
+        >
+          <CloseOutlined />
+        </div>
         <div>{name}</div>
         <div className={styles.basicBar} ref={dom}></div>
       </div>
