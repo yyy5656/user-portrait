7793916209055dace75035c0d8c422307163678a
HunterLi2001 <1398428436@qq.com>
2023-04-23 17:29:38 +0800

    perf: 重构接口列表，添加了新的接口

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,404 +2,446 @@ import QS from "qs";
 import HTTP from "@/service/request";
 
 const api = {
+
   /**
-   * 注册
+   * 选择表项导入数据库
    * @param data
-   * @returns {*}
+   * @returns {HTTP}
    */
-  login(data) {
+  importProperty(data){
     return HTTP({
-      url: "/user/login",
+      url: "/import/importProperty",
       method: "POST",
       headers: {
         "Content-Type": "application/json;charset=UTF-8",
       },
-      data,
+      data
     });
   },
 
   /**
-   * 注册前异步调用的接口，查看注册 username 是否会存在
+   * 获得表项
    * @param data
-   * @returns {*}
-   */
-  checkUsername(data) {
-    return HTTP({
-      url: "/user/checkUsername",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=UTF-8",
-      },
-      data,
-    });
-  },
-
-  /**
-   * 注册
-   * @param data
-   * @returns {*}
-   */
-  register(data) {
-    return HTTP({
-      url: "/user/register",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=UTF-8",
-      },
-      data,
-    });
-  },
-
-  /**
-   * 查看已存在任务
-   * @returns {*}
-   */
-  getConnection() {
-    return HTTP({
-      url: "/user/getConnection",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=UTF-8",
-      },
-    });
-  },
-
-  /**
-   * 删除任务
-   * @param data
-   * @returns {*}
-   */
-  deleteConnection(data) {
-    return HTTP({
-      url: "/user/deleteConnection",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=UTF-8",
-      },
-      data,
-    });
-  },
-
-  /**
-   * 获取已存在 connection 的 token（使用该 token 调用该任务的内容查询）
-   * @param data
-   * @returns {*}
-   */
-  chooseConnection(data) {
-    return HTTP({
-      url: "/user/chooseConnection",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=UTF-8",
-      },
-      data,
-    });
-  },
-
-  /**
-   * 获得指定任务已导入的属性和已导入数据行数
-   * @returns {*}
-   */
-  getLink() {
-    return HTTP({
-      url: "/import/getLink",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=UTF-8",
-      },
-    });
-  },
-
-  /**
-   * 创建任务（与后面接口先后调用）
-   * @param data
-   * @returns {*}
-   */
-  createConnection(data) {
-    return HTTP({
-      url: "/user/createConnection",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=UTF-8",
-      },
-      data,
-    });
-  },
-
-  /**
-   * 上传文件并返回属性字段（使用上一个接口的新 token，前后调用）
-   * @param data
-   * @returns {*}
+   * @returns {HTTP}
    */
   getProperty(data) {
     return HTTP({
-      url: "/import/getProperty",
-      method: "POST",
+      url:"/import/getProperty",
+      method:"POST",
       headers: {
-        "Content-Type": "application/x-www-form-urlencoded", // TODO 试试这个？
+        "Content-Type": "application/x-www-form-urlencoded",
       },
       data,
-    });
+    })
   },
 
   /**
-   * 导入已选属性
-   * 与上一个接口前后调用
-   * 参数来源上个属性的选择，start 最低为 2，end 最高为 dataline 的值
+   * 登录
    * @param data
-   * @returns {*}
+   * @returns {HTTP}
+   */
+  login(data){
+    return HTTP({
+      url:"/user/login",
+      method:"POST",
+      headers: {
+        "Content-Type": "application/json;charset=UTF-8",
+      },
+      data
+    })
+  },
+
+  /**
+   * 创建connection
+   * @param data
+   * @returns {HTTP}
+   */
+  createConnection(data) {
+    return HTTP({
+      url:"/user/createConnection",
+      method:"POST",
+      headers: {
+        "Content-Type": "application/json;charset=UTF-8",
+      },
+      data
+    })
+  },
+
+  /**
+   * 获取所有connection
+   * @returns {HTTP}
+   */
+  getConnection() {
+    return HTTP({
+      url:"/user/getConnection",
+      method:"POST",
+      headers:{
+        "Content-Type": "application/json;charset=UTF-8",
+      }
+    })
+  },
+
+  /**
+   * 删除connection
+   * @param data
+   * @returns {HTTP}
+   */
+  deleteConnection(data) {
+    return HTTP({
+      url:"/user/deleteConnection",
+      method:"POST",
+      headers:{
+        "Content-Type": "application/json;charset=UTF-8",
+      },
+      data
+    })
+  },
+
+  /**
+   * 选择表项和数据行导入数据库
+   * @param data
+   * @returns {HTTP}
    */
   importPropertyByLine(data) {
     return HTTP({
-      url: "/import/importPropertyByLine",
-      method: "POST",
-      headers: {
+      url:"/import/importPropertyByLine",
+      method:"POST",
+      headers:{
         "Content-Type": "application/json;charset=UTF-8",
       },
-      data,
-    });
+      data
+    })
   },
 
   /**
-   * 删除任务的属性
+   * 新增数据
    * @param data
-   * @returns {*}
+   * @returns {HTTP}
+   */
+  import_addDataByLine(data) {
+    return HTTP({
+      url:"/import/addDataByLine",
+      method:"POST",
+      headers:{
+        "Content-Type": "application/json;charset=UTF-8",
+      },
+      data
+    })
+  },
+
+  /**
+   * 检查用户名是否重复
+   * @param data
+   * @returns {HTTP}
+   */
+  checkUsername(data) {
+    return HTTP({
+      url:"/user/checkUsername",
+      method:"POST",
+      headers:{
+        "Content-Type": "application/json;charset=UTF-8",
+      },
+      data
+    })
+  },
+
+  /**
+   * 注册
+   * @param data
+   * @returns {HTTP}
+   */
+  register(data) {
+    return HTTP({
+      url:"/user/register",
+      method:"POST",
+      headers:{
+        "Content-Type": "application/json;charset=UTF-8",
+      },
+      data
+    })
+  },
+
+  /**
+   * 选择connection获得token
+   * @param data
+   * @returns {HTTP}
+   */
+  chooseConnection(data) {
+    return HTTP({
+      url:"/user/chooseConnection",
+      method:"POST",
+      headers:{
+        "Content-Type": "application/json;charset=UTF-8",
+      },
+      data
+    })
+  },
+
+  /**
+   * 获得connection的属性
+   * @returns {HTTP}
+   */
+  getLink() {
+    return HTTP({
+      url:"/import/getLink",
+      method:"POST",
+      headers:{
+        "Content-Type": "application/json;charset=UTF-8",
+      }
+    })
+  },
+
+  /**
+   * 删除属性
+   * @param data
+   * @returns {HTTP}
    */
   removeProperty(data) {
     return HTTP({
-      url: "/import/removeProperty",
-      method: "POST",
-      headers: {
+      url:"/import/removeProperty",
+      method:"POST",
+      headers:{
         "Content-Type": "application/json;charset=UTF-8",
-      },
-      data,
-    });
+      }
+    })
   },
 
   /**
-   * 补充新的属性
-   * 与 9 号接口前后调用，参数来自 9 号接口和 7 接口.
+   * 创建小组
+   * @param data
+   * @returns {HTTP}
+   */
+  createGroup(data){
+    return HTTP({
+      url:"/data/createGroup",
+      method:"POST",
+      headers:{
+        "Content-Type": "application/json;charset=UTF-8",
+      }
+    })
+  },
+
+  /**
+   * 获得connection的所有小组
+   * @returns {HTTP}
+   */
+  getGroups(){
+    return HTTP({
+      url:"/data/getGroups",
+      method:"POST",
+      headers:{
+        "Content-Type": "application/json;charset=UTF-8",
+      }
+    })
+  },
+
+  /**
+   * 删除小组
+   * @param data
+   * @returns {HTTP}
+   */
+  deleteGroup(data){
+    return HTTP({
+      url:"/data/deleteGroup",
+      method:"POST",
+      headers:{
+        "Content-Type": "application/json;charset=UTF-8",
+      },
+      data
+    })
+  },
+
+  /**
+   * link属性获得对应全部数据
+   * @param data
+   * @returns {HTTP}
+   */
+  getDataByLinks(data) {
+    return HTTP({
+      url:"/data/getDataByLinks",
+      method:"POST",
+      headers:{
+        "Content-Type": "application/json;charset=UTF-8",
+      },
+      data
+    })
+  },
+
+
+  /**
+   * 获得全部数据
+   * @returns {HTTP}
+   */
+  getAllData() {
+    return HTTP({
+      url:"/data/getAllData",
+      method:"POST",
+      headers:{
+        "Content-Type": "application/json;charset=UTF-8",
+      }
+    })
+  },
+
+  // /**
+  //  * 查询后单条更新信息
+  //  * @param data
+  //  * @returns {HTTP}
+  //  */
+  // updateDataById(data){
+  //     return HTTP({
+  //         url:"/data/updateDataById",
+  //         method:"POST",
+  //         headers:{
+  //             "Content-Type": "application/json;charset=UTF-8",
+  //         },
+  //         data
+  //     })
+  // },
+
+  /**
+   * 新增数据（属性）
+   * @param data
+   * @returns {HTTP}
+   */
+  addData(data){
+    return HTTP({
+      url:"/import/addData",
+      method:"POST",
+      headers:{
+        "Content-Type": "application/json;charset=UTF-8",
+      },
+      data
+    })
+  },
+
+  /**
+   * 处理名称数据分组
+   * @param data
+   * @returns {HTTP}
+   */
+  getNouns(data) {
+    return HTTP({
+      url:"/data/getNouns",
+      method:"POST",
+      headers:{
+        "Content-Type": "application/json;charset=UTF-8",
+      },
+      data
+    })
+  },
+
+  /**
+   * 转换：名词属性/数字属性
+   * @param data
+   * @returns {HTTP}
+   */
+  changeLinkType(data){
+    return HTTP({
+      url:"/data/changeLinkType",
+      method:"POST",
+      headers:{
+        "Content-Type": "application/json;charset=UTF-8",
+      },
+      data
+    })
+  },
+
+  /**
+   * 处理数值属性分类
+   * @param data
+   * @returns {HTTP}
+   */
+  getNumerical(data){
+    return HTTP({
+      url:"/data/getNumerical",
+      method:"POST",
+      headers:{
+        "Content-Type": "application/json;charset=UTF-8",
+      },
+      data
+    })
+  },
+
+  /**
+   * 根据属性分组拿到全部属性
+   * @param data
+   * @returns {HTTP}
+   */
+  getLinksByType(data) {
+    return HTTP({
+      url:"/data/getLinksByType",
+      method:"POST",
+      headers:{
+        "Content-Type": "application/json;charset=UTF-8",
+      },
+      data
+    })
+  },
+
+  /**
+   * 更新数据
+   * @param data
+   * @returns {HTTP}
+   */
+  updateData(data){
+    return HTTP({
+      url:"/import/updateData",
+      method:"POST",
+      headers:{
+        "Content-Type": "application/json;charset=UTF-8",
+      },
+      data
+    })
+  },
+
+  /**
+   * 单行更新数据(数据格式来自接口获得全部数据)
+   * @param data
+   * @returns {HTTP}
+   */
+  updateDataById(data){
+    //     return HTTP({
+    //         url:"/import/updateDataById",
+    //         method:"POST",
+    //         headers:{
+    //             "Content-Type":"application/json;charset=UTF-8"
+    //         },
+    //         data
+    //     })
+  },
+
+  /**
+   * 手动新增数据
    * @param data
    * @returns {*}
    */
-  addData(data) {
+  data_addDataByLine(data){
     return HTTP({
-      url: "/import/addData",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=UTF-8",
+      url:"/data/addDataByLine",
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json;charset=UTF-8"
       },
-      data,
-    });
-  },
-  updateData(data) {
-    return HTTP({
-      url: "/import/updateData",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=UTF-8",
-      },
-      data,
-    });
+      data
+    })
   },
 
-  // createGroup(data) {
-  //     return HTTP({
-  //         url: "/data/createGroup",
-  //         method: "POST",
-  //         headers: {
-  //             "Content-Type": "application/json;charset=UTF-8"
-  //         },
-  //         data
-  //     });
-  // },
-  // deleteGroup(data) {
-  //     return HTTP({
-  //         url: "/data/deleteGroup",
-  //         method: "POST",
-  //         headers: {
-  //             "Content-Type": "application/json;charset=UTF-8"
-  //         },
-  //         data
-  //     });
-  // },
-  // getGroups(data) {
-  //     return HTTP({
-  //         url: "/data/getGroups",
-  //         method: "POST",
-  //         headers: {
-  //             "Content-Type": "application/json;charset=UTF-8"
-  //         },
-  //         data
-  //     });
-  // },
-  //
-  // getAllData(data) {
-  //     return HTTP({
-  //         url: "/data/getAllData",
-  //         method: "POST",
-  //         headers: {
-  //             "Content-Type": "application/json;charset=UTF-8"
-  //         },
-  //         data
-  //     });
-  // },
-  //  getDataByLinks(data) {
-  //     return HTTP({
-  //         url: "/data/getDataByLinks",
-  //         method: "POST",
-  //         headers: {
-  //             "Content-Type": "application/json;charset=UTF-8"
-  //         },
-  //         data
-  //     });
-  // }
+  /**
+   * 删除单条数据
+   * @param data
+   * @returns {HTTP}
+   */
+  deleteDataById(data){
+    return HTTP({
+      url:"/data/deleteDataById",
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json;charset=UTF-8"
+      },
+      data
+    })
+  }
 
-  getAllData(data) {
-    return HTTP({
-      url: "/data/getAllData",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=UTF-8",
-      },
-      data,
-    });
-  },
-  getDataByLinks(data) {
-    return HTTP({
-      url: "/data/getDataByLinks",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=UTF-8",
-      },
-      data,
-    });
-  },
-  getLinksByType() {
-    return HTTP({
-      url: "/data/getLinksByType",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=UTF-8",
-      },
-    });
-  },
-  getNouns(data) {
-    return HTTP({
-      url: "/data/getNouns",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=UTF-8",
-      },
-      data,
-    });
-  },
-  getNumerical(data) {
-    return HTTP({
-      url: "/data/getNumerical",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=UTF-8",
-      },
-      data,
-    });
-  },
-  insertViewInfo(data) {
-    return HTTP({
-      url: "/data/insertViewInfo",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=UTF-8",
-      },
-      data,
-    });
-  },
-  getViewInfo() {
-    return HTTP({
-      url: "/data/getViewInfo",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=UTF-8",
-      },
-    });
-  },
-  deleteViewInfo(data) {
-    return HTTP({
-      url: "/data/deleteViewInfo",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=UTF-8",
-      },
-      data,
-    });
-  },
-  getSharedConnection() {
-    return HTTP({
-      url: "/share/getSharedConnection",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=UTF-8",
-      },
-    });
-  },
-  getConnectionsByType(data) {
-    return HTTP({
-      url: "/share/getConnectionsByType",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=UTF-8",
-      },
-      data,
-    });
-  },
-  getOtherALlUser() {
-    return HTTP({
-      url: "/share/getOtherALlUser",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=UTF-8",
-      },
-    });
-  },
-  getShareList() {
-    return HTTP({
-      url: "/share/getShareList",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=UTF-8",
-      },
-    });
-  },
-  changeConnectionType(data) {
-    return HTTP({
-      url: "/share/changeConnectionType",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=UTF-8",
-      },
-      data,
-    });
-  },
-  deleteShareById(data) {
-    return HTTP({
-      url: "/share/deleteShareById",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=UTF-8",
-      },
-      data,
-    });
-  },
-  addDataByLine(data) {
-    return HTTP({
-      url: "/data/addDataByLine",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=UTF-8",
-      },
-      data,
-    });
-  },
 };
 
 export default api;
